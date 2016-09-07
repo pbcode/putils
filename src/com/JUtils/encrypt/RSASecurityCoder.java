01fc5d75b454ed74a9d3c61fb13d6dbbb08a5819
287746074 <287746074@qq.com>
2016-09-07 13:20:37 +0800

    添加2个RSA加密

---
+++ src/com/JUtils/encrypt/RSASecurityCoder.java
@@ -0,0 +1,127 @@
+package com.JUtils.encrypt;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+import org.apache.commons.codec.binary.Base64;
+/**
+ * RSA加密解密类 说明： 作者：何杨(heyang78@gmail.com) 创建时间：2010-12-1 下午06:14:38
+ * 修改时间：2010-12-1 下午06:14:38
+ */
+@SuppressWarnings("restriction")
+public class RSASecurityCoder {
+	// 非对称加密密钥算法
+	private static final String Algorithm = "RSA";
+
+	// 密钥长度，用来初始化
+	private static final int Key_Size = 1024;
+
+	// 公钥
+	private final byte[] publicKey;
+
+	// 私钥
+	private final byte[] privateKey;
+
+	/**
+	 * 构造函数，在其中生成公钥和私钥
+	 * 
+	 * @throws Exception
+	 */
+	public RSASecurityCoder() throws Exception {
+		// 得到密钥对生成器
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance(Algorithm);
+		kpg.initialize(Key_Size);
+		// 得到密钥对
+		KeyPair kp = kpg.generateKeyPair();
+
+		// 得到公钥
+		RSAPublicKey keyPublic = (RSAPublicKey) kp.getPublic();
+		publicKey = keyPublic.getEncoded();
+
+		// 得到私钥
+		RSAPrivateKey keyPrivate = (RSAPrivateKey) kp.getPrivate();
+		privateKey = keyPrivate.getEncoded();
+	}
+
+	/**
+	 * 用公钥对字符串进行加密
+	 * 
+	 * 说明：
+	 * 
+	 * @param originalString
+	 * @param publicKeyArray
+	 * @return
+	 * @throws Exception
+	 *             创建时间：2010-12-1 下午06:29:51
+	 */
+	public byte[] getEncryptArray(String originalString, byte[] publicKeyArray) throws Exception {
+		// 得到公钥
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyArray);
+		System.out.println(Base64.encodeBase64String(keySpec.getEncoded()));
+		KeyFactory kf = KeyFactory.getInstance(Algorithm);
+		PublicKey keyPublic = kf.generatePublic(keySpec);
+
+		// 加密数据
+		Cipher cp = Cipher.getInstance(Algorithm);
+		cp.init(Cipher.ENCRYPT_MODE, keyPublic);
+		return cp.doFinal(originalString.getBytes());
+	}
+
+	/**
+	 * 使用私钥进行解密
+	 * 
+	 * 说明：
+	 * 
+	 * @param encryptedDataArray
+	 * @return
+	 * @throws Exception
+	 *             创建时间：2010-12-1 下午06:35:28
+	 */
+	public String getDecryptString(byte[] encryptedDataArray) throws Exception {
+		// 得到私钥
+		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKey);
+		KeyFactory kf = KeyFactory.getInstance(Algorithm);
+		System.out.println(Base64.encodeBase64String(keySpec.getEncoded()));
+		PrivateKey keyPrivate = kf.generatePrivate(keySpec);
+		// 解密数据
+		Cipher cp = Cipher.getInstance(Algorithm);
+		cp.init(Cipher.DECRYPT_MODE, keyPrivate);
+		byte[] arr = cp.doFinal(encryptedDataArray);
+
+		// 得到解密后的字符串
+		return new String(arr);
+	}
+
+	public byte[] getPublicKey() {
+		return publicKey;
+	}
+
+	public static void main(String[] arr) throws Exception {
+		String str = "你好，世界！ Hello,world!";
+		System.out.println("准备用公钥加密的字符串为：" + str);
+
+		// 用公钥加密
+		RSASecurityCoder rsaCoder = new RSASecurityCoder();
+		byte[] publicKey = rsaCoder.getPublicKey();
+		byte[] encryptArray = rsaCoder.getEncryptArray(str, publicKey);
+
+		System.out.print("用公钥加密后的结果为:");
+		for (byte b : encryptArray) {
+			System.out.print(b);
+		}
+		System.out.println();
+
+		// 用私钥解密
+		String str1 = rsaCoder.getDecryptString(encryptArray);
+		System.out.println("用私钥解密后的字符串为：" + str1);
+	}
+}
---
+++ src/com/JUtils/encrypt/RSAUtil.java
@@ -0,0 +1,222 @@
+package com.JUtils.encrypt;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class RSAUtil {
+	private static final String AU = "RSA";
+	private static final int SIZE = 1024;
+	private static byte[] publicKey;
+	private static byte[] privateKey;
+
+	private static void getKeyPair() {
+		try {
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance(AU);
+			kpg.initialize(SIZE);
+			KeyPair kp = kpg.generateKeyPair();
+			PublicKey uk = kp.getPublic();
+			PrivateKey pk = kp.getPrivate();
+			publicKey = uk.getEncoded();
+			privateKey = pk.getEncoded();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String getUk() {
+		getKeyPair();
+		return Base64.encodeBase64String(publicKey);
+	}
+
+	public static String getPk() {
+		return Base64.encodeBase64String(privateKey);
+	}
+
+	public static String encode(String msg, String str_uk) {
+		try {
+			StringBuffer sb = new StringBuffer();
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(str_uk));
+			Cipher cp = Cipher.getInstance(AU);
+			KeyFactory kf = KeyFactory.getInstance(AU);
+			PublicKey uk = kf.generatePublic(keySpec);
+			cp.init(Cipher.ENCRYPT_MODE, uk);
+			byte[] byte_msg = msg.getBytes();
+			for (int i = 0; i < byte_msg.length; i += 100) {
+				byte[] msg_password = cp.doFinal(ArrayUtils.subarray(byte_msg, i, i + 100));
+				for (byte b : msg_password) {
+					sb.append(b);
+					sb.append(",");
+				}
+			}
+			String returnStr = sb.toString();
+			if (StringUtils.endsWith(returnStr, ",")) {
+				returnStr = StringUtils.removeEnd(returnStr.toString(), ",");
+			}
+			return returnStr;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String encode(byte[] msg, String str_uk) {
+		try {
+			StringBuffer sb = new StringBuffer();
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(str_uk));
+			Cipher cp = Cipher.getInstance(AU);
+			KeyFactory kf = KeyFactory.getInstance(AU);
+			PublicKey uk = kf.generatePublic(keySpec);
+			cp.init(Cipher.ENCRYPT_MODE, uk);
+			for (int i = 0; i < msg.length; i += 100) {
+				byte[] msg_password = cp.doFinal(ArrayUtils.subarray(msg, i, i + 100));
+				for (byte b : msg_password) {
+					sb.append(b);
+					sb.append(",");
+				}
+			}
+			String returnStr = sb.toString();
+			if (StringUtils.endsWith(returnStr, ",")) {
+				returnStr = StringUtils.removeEnd(returnStr.toString(), ",");
+			}
+			return returnStr;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String decode(String msg_password, String pk) {
+		try {
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(pk));
+			Cipher cp = Cipher.getInstance(AU);
+			KeyFactory kf = KeyFactory.getInstance(AU);
+			cp.init(Cipher.DECRYPT_MODE, kf.generatePrivate(keySpec));
+			StringBuffer sb = new StringBuffer();
+			String[] str_byte_array = StringUtils.split(msg_password, ",");
+			byte[] byte_msg_password = new byte[str_byte_array.length];
+			for (int i = 0; i < str_byte_array.length; i++) {
+				byte_msg_password[i] = Byte.parseByte(str_byte_array[i]);
+			}
+			for (int i = 0; i < byte_msg_password.length; i += 128) {
+				byte[] msg_byte = cp.doFinal(ArrayUtils.subarray(byte_msg_password, i, i + 128));
+				sb.append(new String(msg_byte));
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String decode(byte[] msg_password) {
+		try {
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(getPk()));
+			Cipher cp = Cipher.getInstance(AU);
+			KeyFactory kf = KeyFactory.getInstance(AU);
+			cp.init(Cipher.DECRYPT_MODE, kf.generatePrivate(keySpec));
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < msg_password.length; i += 128) {
+				byte[] msg = cp.doFinal(ArrayUtils.subarray(msg_password, i, i + 128));
+				sb.append(new String(msg));
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static byte[] encodeFile(File file, String str_uk) {
+		byte[] buffer = null;
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			byte[] b = new byte[1024];
+			int n;
+			while ((n = fis.read(b)) != -1) {
+				bos.write(b, 0, n);
+			}
+			buffer = bos.toByteArray();
+			String content = encode(buffer, str_uk);
+			File ff = new File("D:\\1m_mssage");
+			if (!ff.exists()) {
+				ff.createNewFile();
+			}
+			FileWriter writer = new FileWriter(ff);
+			BufferedWriter bufferWritter = new BufferedWriter(writer);
+			bufferWritter.write(content);
+			bufferWritter.close();
+			fis.close();
+			bos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+
+	public static byte[] decodeFile(File file, String pk) {
+		byte[] buffer = null;
+		Reader reader = null;
+		try {
+			reader = new InputStreamReader(new FileInputStream(file));
+			int tempchar;
+			StringBuffer sb = new StringBuffer();
+			while ((tempchar = reader.read()) != -1) {
+				if (((char) tempchar) != '\r') {
+					sb.append((char) tempchar);
+				}
+			}
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			File ff = new File("D:\\1m_ms.txt");
+			if (!ff.exists()) {
+				ff.createNewFile();
+			}
+			String content = decode(sb.toString(), pk);
+			FileWriter writer = new FileWriter(ff);
+			BufferedWriter bufferWritter = new BufferedWriter(writer);
+			bufferWritter.write(content);
+			bufferWritter.close();
+			reader.close();
+			bos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+
+	public static void main(String[] args) {
+		String uk = RSAUtil.getUk();
+		String pk = RSAUtil.getPk();
+//		System.out.println(pk);
+//		File file = new File("D:\\1m.txt");
+//		RESUtil.encodeFile(file, uk);
+//		String pk="MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIj0GdbpG6JPVjBYW6uSFfA5pI6OhKAX6yafZGR/huWR6u04ttb74VNqYvzP9yHoV+x+fEE84Emi4OzDDAuX8+6YwXyw9si09j7O+mPgV8ZmDOslbOJsL0w0n3t1e80hV7BpGy00MpdB+8NnhPA6RaxksGtib6+41eZAe7LEMa7lAgMBAAECgYA77LMxuiYBSz2nQcI6vF0lE9PRHMMjWdPmvm+rGbHo2YQ18E7wmp3pZe+SN10nVQbD0ESGNyDcl4xbjKzEleSBqd+24AqftCcJb0AY4sU5hE9QZ+iv/TCWmekERqxw/j7bd4b51+/1ygk+SETVKfhZ4MQDMNs84SRj54neAKjDMQJBAMdEoS4eMNWKt+g9oFi4q+abzezYc38ItnYnaFUFE/lF7PpXXbJEVk6rf1ywKbd9v9ioPQP+/bdOJND8Redqi1sCQQCv8cNWjZsiGb0NSivh6Vcwn759vS20VQJHKuxQRalifbupl430eFOAX7memqnh9+suGYAirUdNnZCM1cGvZwK/AkAI3ogiClv4FT2MS7noWQflsseepB+35sZgSe694gT6kl8y4VKdTaddxwpbMMgaj7FLTmmw1NesIgFHgAMIgtiDAkEAnrpJM6iEr/rwZzDm7eQI2MEEpYMp3Gpkp7e9gZ2W9lfgGVu0oTx8eG/jkYaOhGGZNfmjc6VHkmn3olalaYxRGQJAXlSmnVYBl+EolhYi574L9l3JOevvVD0y3X2YVGiGrgubEEOYQwSa9OPbIqqYhReSmejoEfMb1fYLQxt7TZ4B/A==";
+//		File file = new File("D:\\1m_mssage");
+//		RESUtil.decodeFile(file,pk);
+		 System.out.println("加密前数据为：helloworld");
+		 System.out.println("公钥为:" + uk);
+		 String msg_password = RSAUtil.encode("helloworld",uk);
+		 System.out.println("加密后数据为："+msg_password);
+		 System.out.println("解密后数据为："+RSAUtil.decode(msg_password,pk));
+	}
+}
